b5b7b99cefebba74a8c345a792e51378b2a9e6aa
D15123660 <33661638+D15123660@users.noreply.github.com>
2020-11-11 23:25:37 +0000

    ca1

---
+++ WebMappingCA1/web/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+import datetime
+
+from django.contrib.gis.db import models
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.PointField()
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
